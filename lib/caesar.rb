c87723e6e6cbe6e3ebdd9e7634cb86831fc11a4c
christy8059 <christyl8059@hstat.org>
2017-12-08 02:49:16 +0000

    removed prompts

--- lib/caesar.rb
+++ lib/caesar.rb
@@ -15,11 +15,6 @@ def caesar_encode(string,offset)
   end.join ("")                                                                 
 end
 
-puts "What would you like to encode?"
-user_string = gets.chomp
-puts "What do you want the offset to be?"
-user_offset = gets.chomp.to_i
-puts caesar_encode(user_string,user_offset)
 
 #(Index + off set) % 26 
 
@@ -39,14 +34,8 @@ def caesar_decode(string,offset)
 #       character                                                               
 #     end 
 #   end.join ("")                                                                
-
 end
 
-puts "What would you like to decode?"
-user_string = gets.chomp
-puts "What do you want the offset to be?"
-user_offset = gets.chomp.to_i
-puts caesar_decode(user_string,user_offset)
 
 
 
